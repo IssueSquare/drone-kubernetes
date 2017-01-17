b11dd57338c18e075f6db296292b67f49a3902b9
Charles Martinot <charles.martinot@honestbee.com>
2017-01-17 11:04:10 +0800

    Remove name option

--- update.sh
+++ update.sh
@@ -8,4 +8,4 @@ kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
 kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
-kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${PLUGIN_DEPLOYMENT} ${PLUGIN_NAME}=${PLUGIN_REPO}:${PLUGIN_TAG}
+kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${PLUGIN_DEPLOYMENT} ${PLUGIN_CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG}

995b37fe1a26ca86e8b8349a2d685536eb7e0924
Charles Martinot <charles.martinot@honestbee.com>
2017-02-08 14:38:37 +0800

    Arrays are converted to strings

--- update.sh
+++ update.sh
@@ -17,7 +17,7 @@ kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
 
-DEPLOYMENTS=$(echo $PLUGIN_DEPLOYMENT|tr -d '[],')
+IFS=', ' read -r -a DEPLOYMENTS <<< "$PLUGIN_DEPLOYMENT"
 for DEPLOY in ${DEPLOYMENTS[@]}; do
   kubectl -n ${PLUGIN_NAMESPACE} set image deployment/${DEPLOY} \
     ${PLUGIN_CONTAINER}=${PLUGIN_REPO}:${PLUGIN_TAG}

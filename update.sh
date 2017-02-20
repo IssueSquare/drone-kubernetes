cfc612a6214d743bd369cd285b166a523e5d0b78
Vincent De Smet <vincent.desmet@honestbee.com>
2017-02-20 17:22:24 +0800

    Add support for TLS Verification

--- update.sh
+++ update.sh
@@ -6,14 +6,20 @@ fi
 
 if [ ! -z ${PLUGIN_KUBERNETES_TOKEN} ]; then
   KUBERNETES_TOKEN=$PLUGIN_KUBERNETES_TOKEN
+  kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
 fi
 
 if [ ! -z ${PLUGIN_KUBERNETES_SERVER} ]; then
   KUBERNETES_SERVER=$PLUGIN_KUBERNETES_SERVER
 fi
 
-kubectl config set-credentials default --token=${KUBERNETES_TOKEN}
-kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
+if [ ! -z ${PLUGIN_KUBERNETES_CERT} ]; then
+  echo ${PLUGIN_KUBERNETES_CERT} > ca.crt
+  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --certificate-authority=ca.crt
+else
+  kubectl config set-cluster default --server=${KUBERNETES_SERVER} --insecure-skip-tls-verify=true
+fi
+
 kubectl config set-context default --cluster=default --user=default
 kubectl config use-context default
 
